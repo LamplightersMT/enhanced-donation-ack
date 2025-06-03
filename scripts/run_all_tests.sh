a45d1e99d1497cc4910ce57ae3bcd08accb5435f
Chris Uzelac <cuzelac@gmail.com>
2025-06-02 21:16:23 -0700

    Add basic tests

---
+++ scripts/run_all_tests.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Run all Salesforce Apex tests and all LWC Jest tests
+
+set -e
+
+# Run all Jest (LWC) tests
+npm run test:unit
+
+# Run all Apex tests in the default org
+sf apex test run --wait 10 --result-format human
